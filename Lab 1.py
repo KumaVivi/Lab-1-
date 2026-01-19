0527dc74a67908090cc7a9e225e7f62f2a2a3fbe
KumaVivi <162097312+KumaVivi@users.noreply.github.com>
2026-01-19 13:34:14 -0500

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,12 @@
+def hello_world():
+    print("Hello babygirl")
+hello_world()
+#inputs
+def input_output_functions():
+    name = input("Enter your name: ")
+    age=int(input ("Enter your age: "))
+    height=float(input ("Enter your height: "))
+    print(f"Your name is {name}, you are {age} years old and your height is {height}m")
+input_output_functions()
+
+
